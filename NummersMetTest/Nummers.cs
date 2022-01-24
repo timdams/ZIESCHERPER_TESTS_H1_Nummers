8bb3ff52eb4586a6d2131867bd94a70a011de30e
Tim Dams <dams.tim@telenet.be>
2022-01-24 16:19:05 +0100

    Update Nummers.cs

--- NummersMetTest/Nummers.cs
+++ NummersMetTest/Nummers.cs
@@ -9,22 +9,5 @@ namespace NummersApplicatie
     public class Nummers
     {
 
-
-        public int Getal1 { get; set; }
-        public int Getal2 { get; set; }
-
-        public int Som() { return Getal1 + Getal2; }
-        public int Verschil() { return Getal1 - Getal2; }
-        public int Product() { return Getal1 * Getal2; }
-
-        public double Quotient()
-        {
-            if (Getal2 == 0)
-            {
-                Console.WriteLine("Kan niet delen door 0");
-                return 0;
-            }
-            return Getal1 / (double)Getal2;
-        }
     }
 }
